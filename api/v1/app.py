c6896cf2142690c4d25a7b0fa495559f51c52ae6
Brian <chumobrian@gmail.com>
2024-02-04 17:42:51 +0300

    Fix: Add a jsonify module import statement

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Defines a module that calls app.py """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
