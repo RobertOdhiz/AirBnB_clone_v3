653da90003ffe25c9398e2411666e4aecbaba883
Brian <chumobrian@gmail.com>
2024-02-04 18:12:26 +0300

    Add a State view for State objects that handles all default RESTFul API actions

---
+++ api/v1/views/states.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" defines a module that Create a new view for State
+objects that handles all default RESTFul API actions """
+from flask import jsonify
+from api.v1.views import app_views
+
