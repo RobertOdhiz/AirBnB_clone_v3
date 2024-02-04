3d6cee72649e2c96427068849ece030a438463e1
Brian <chumobrian@gmail.com>
2024-02-04 17:19:44 +0300

    Add a doc string

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,10 +18,12 @@ def clear_resource(exception=None):
 
 @app.errorhandler(404)
 def not_found(error):
+    """ Handles 404 not found"""
     return jsonify({'error': "not found"})
 
 
 if __name__ == "__main__":
+    """ """
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', 5000)
     app.run(debug=True, host=host, port=port, threaded=True)
