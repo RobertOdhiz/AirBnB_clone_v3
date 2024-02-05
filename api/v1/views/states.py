f816e7e70c18e96f2d483fd96ca3fb50395a61c3
Brian <chumobrian@gmail.com>
2024-02-05 12:39:54 +0300

    feat(api): add a view to handle the State class

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,24 +7,31 @@ from models.state import State
 from models import storage
 
 
+def get_json_file():
+    """Returns a json file"""
+    try:
+        if not request.is_json:
+            abort(400, description="Not a JSON")
+        req = request.get_json()
+    except Exception as e:
+        abort(400, description="Not a JSON")
+    return req
+
+
 @app_views.route('/states', strict_slashes=False, methods=['POST', 'GET'])
 def get_states():
     """Gets the list of all State objects """
     if request.method == 'GET':
         states = list(storage.all(State).values())
         states_dict = [state.to_dict() for state in states]
-
-        return states_dict
-    elif request.method == 'POST':
-        if not request.is_json:
-            return jsonify({'error', 'Not a JSON'}), 400
-        req = request.get_json()
+        return jsonify(states_dict)
+    if request.method == 'POST':
+        req = get_json_file()
         if not req.get('name'):
-            return jsonify({'error', 'Missing name'}), 400
+            abort(400, description="Missing name")
         state = State(name=req.get('name'))
         state.save()
-
-        return jsonify(state.to_dict())
+        return jsonify(state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
@@ -46,22 +53,17 @@ def del_state(state_id):
             abort(404)
         storage.delete(state)
         storage.save()
-
         return jsonify({})
-    elif request.method == 'PUT':
+
+    if request.method == 'PUT':
         state = storage.get(State, state_id)
         if state is None:
             abort(404)
-        if not request.is_json:
-            return jsonify({'error', 'Not a JSON'}), 400
-
-        req = request.get_json()
-        req.pop('id', None)
-        req.pop('created_at', None)
-        req.pop('updated_at', None)
-
+        req = get_json_file()
+        keys = ['id', 'created_at', 'updated_at']
         for key, value in req.items():
-            setattr(state, key, value)
+            if not key in keys:
+                setattr(state, key, value)
 
         storage.save()
 
