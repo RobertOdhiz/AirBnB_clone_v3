ef015e75aaae4c1c2381b09a5820f8b47379ed03
Brian <chumobrian@gmail.com>
2024-02-05 16:35:34 +0300

    feat(app): Add a CORS to allow '0.0.0.0' or all hosts to the resource '/*'

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """ Defines a module that calls app.py """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
